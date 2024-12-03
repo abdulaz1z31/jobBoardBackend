1817e08004dc62816ad18f63dd205c0cde61e546
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2024-12-03 12:16:40 +0500

    fixed auth problmes

--- migrations/20241128073357_users.js
+++ migrations/20241128073357_users.js
@@ -5,11 +5,12 @@
 export async function up(knex) {
     await knex.schema.createTable('users', function (table) {
         table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'))
-        table.string('name'), table.string('username')
-        table.string('email').notNullable()
+        table.string('name'), 
+        table.string('username').unique()
+        table.string('email').notNullable().unique()
         table.string('password').notNullable()
         table
-            .enum('role', ['admin', 'job_seeker', 'recruiter'])
+            .enum('role', ['superAdmin', 'admin', 'job_seeker', 'recruiter'])
             .defaultTo('job_seeker')
         table.enum('status', ['active', 'inactive']).defaultTo('inactive')
         table.timestamps(true, true)
--- src/middleware/checkTokens.js
+++ src/middleware/checkTokens.js
@@ -1,4 +1,4 @@
-import { verifyTokens } from '../helpers/index.helpers.js'
+import { verifyForgetToken, verifyTokens } from '../helpers/index.helpers.js'
 import { logger, statusCode } from '../utils/index.utils.js'
 
 export const checkToken = (req, res, next) => {
@@ -12,6 +12,34 @@ export const checkToken = (req, res, next) => {
         }
         const token = bearerToken.split(' ')[1]
         const decode = verifyTokens('access', token)
+        const status = decode.status
+        if (status != 'active') {
+            throw new Error("Account is not active");
+        }
+        req.user = decode
+        next()
+    } catch (err) {
+        logger.error('Error in token verification:', err)
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({
+            error: 'Server error while verifying token',
+        })
+    }
+}
+export const checkForgetToken = (req, res, next) => {
+    try {
+        const bearerToken = req.headers?.authorization
+
+        if (!bearerToken || !bearerToken.startsWith('Bearer')) {
+            return res
+                .status(statusCode.UNAUTHORIZED)
+                .send('Authentication is required in bearer token')
+        }
+        const token = bearerToken.split(' ')[1]
+        const decode = verifyForgetToken(token)
+        const status = decode.status
+        if (status != 'active') {
+            throw new Error("Account is not active");
+        }
         req.user = decode
         next()
     } catch (err) {
