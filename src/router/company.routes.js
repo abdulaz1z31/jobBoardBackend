935a8788a3639006044ebb5636b8531031a41f27
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2024-12-03 21:53:23 +0500

    fixe guard check

--- src/router/company.routes.js
+++ src/router/company.routes.js
@@ -26,4 +26,4 @@ companyRouter.get(
 companyRouter.post('/', checkToken, createCompanyController)
 companyRouter.put('/:id', checkToken, updateIdCompanyController)
 companyRouter.delete('/:id', checkToken, deleteCompanyController)
-// company
+
